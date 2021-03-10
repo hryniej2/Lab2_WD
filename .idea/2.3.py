fff85387f78c7b5d5730888d1ae8e5d55a221fa4
Jakub <jakubhryniewicki21@gmail.com>
2021-03-10 12:08:04 +0100

    Lab 2

--- .idea/2.3.py
+++ .idea/2.3.py
@@ -1,3 +1,3 @@
-Przedmioty = {"CAD": "CAD komputerowe wspomaganie projektowania", "PS": "Programowanie strukturalne", "WOT": "Wiedza o teatrze",
+Przedmioty_letnie = {"CAD": "CAD komputerowe wspomaganie projektowania", "PS": "Programowanie strukturalne", "WOT": "Wiedza o teatrze",
               "AM": "Analiza matematyczna dla informatyków", "WD": "Wizualizacja danych", "MD": "Matematyka dyskretna dla informatyków"}
-print(len(Przedmioty))
+print(len(Przedmioty_letnie))
