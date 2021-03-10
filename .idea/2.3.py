a8a1e9da787a170ac57fea023c521878cd5d2e6d
Jakub <jakubhryniewicki21@gmail.com>
2021-03-10 12:06:30 +0100

    Lab 2

---
+++ .idea/2.3.py
@@ -0,0 +1,3 @@
+Przedmioty = {"CAD": "CAD komputerowe wspomaganie projektowania", "PS": "Programowanie strukturalne", "WOT": "Wiedza o teatrze",
+              "AM": "Analiza matematyczna dla informatyków", "WD": "Wizualizacja danych", "MD": "Matematyka dyskretna dla informatyków"}
+print(len(Przedmioty))
