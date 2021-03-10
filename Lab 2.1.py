7c641ef83ce9fb811d31555af039d55a5e469857
Jakub <jakubhryniewicki21@gmail.com>
2021-03-10 11:21:12 +0100

    Lab 2

---
+++ Lab 2.1.py
@@ -0,0 +1,9 @@
+Filmy = ["Transformers 1", "Transformers 2 Zemsta upadłych", "Transformers 3", "Transformers 4 Wiek zagłady",
+         "Transformers 5 Ostatni rycerz"]
+Filmy.reverse()
+Filmy.append("Star Wars Mroczne widmo")
+Filmy.append("Star Wars Wojny klonów")
+Filmy.append("Star Wars Zemsta Sithów")
+Filmy.append("Star Wars Nowa nadzieja")
+Filmy.append("Star Wars Powrót Jedi")
+print(Filmy)
