cc65cc8f0062bc9f36c84b12f0a32704e4ff42f7
Jakub <jakubhryniewicki21@gmail.com>
2021-03-10 11:53:41 +0100

    Lab 2

---
+++ Lab 2.2.py
@@ -0,0 +1,6 @@
+Filmy = {2017: "Transformers 5 Ostatni rycerz", 2014: "Transformers 4 Wiek zagłady",
+2011: 'Transformers 3', 2009: 'Transformers 2 Zemsta upadłych', 2007: 'Transformers 1', 1999: 'Star Wars Mroczne widmo',
+2002: 'Star Wars Wojny klonów', 2005: 'Star Wars Zemsta Sithów', 1977: 'Star Wars Nowa nadzieja', 1983: 'Star Wars Powrót Jedi'}
+
+
+print(Filmy[1977])
